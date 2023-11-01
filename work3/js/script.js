a72a02ae082abc6e20b3cc960293797369d3c531
izumi-numayama <duskclown4@gmail.com>
2023-11-01 18:20:11 +0900

    work3.2

--- work3/js/script.js
+++ work3/js/script.js
@@ -1,8 +1,11 @@
 //work1
 let num = 1;
-for(i = 0; i < 11; i++){
-    num = num +i;
+let total = 0;
+for(i = 0; i < 10; i++){
+    total = total + num;
+    num = num +1;
 }
+console.log(total);
 if(num >= 20){
    console.log("20以上");
 }else if(num < 20){
